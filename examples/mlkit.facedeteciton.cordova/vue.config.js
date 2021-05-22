4bdef783a5de22d5021ae9877b3d1d574fa89c51
rai.gyoumei <rai.gyoumei@tripodw.jp>
2021-05-22 17:30:45 +0900

    configファイルの修正

--- examples/mlkit.facedeteciton.cordova/vue.config.js
+++ examples/mlkit.facedeteciton.cordova/vue.config.js
@@ -1,6 +1,16 @@
+const path = require('path');
+
 module.exports = {
   publicPath: '',
   pluginOptions: {
     cordovaPath: 'src-cordova'
+  },
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@src': path.join(__dirname, '/src') // @の参照先の変更
+      }
+    }
   }
 }
